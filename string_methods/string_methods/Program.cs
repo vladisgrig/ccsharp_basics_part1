7276cd652b8f2600cc55bd80fdb03937ed49a1dd
vladisgrig <vladisgrig@yandex.ru>
2016-11-21 00:24:31 +0300

    String methods

---
+++ string_methods/string_methods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetLastHalf("I love CSharp!"));
+            Console.WriteLine(GetLastHalf("1234567890"));
+            Console.WriteLine(GetLastHalf("до ре ми фа соль ля си"));
+        }
+
+        static string GetLastHalf(string text)
+        {
+            int length = text.Length;
+            string right_half = text.Substring(length / 2);
+            return right_half.Replace(" ", "");
+        }
+    }
+}
