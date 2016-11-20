c449d329d802f1c172db6a6252cac2606607bb74
vladisgrig <vladisgrig@yandex.ru>
2016-11-21 00:17:40 +0300

    Math.Ceiling

---
+++ good_employer/good_employer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace good_employer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PrintGreeting("Student", 10.01));
+            Console.WriteLine(PrintGreeting("Bill Gates", 10000000.5));
+            Console.WriteLine(PrintGreeting("Steve Jobs", 1));
+        }
+
+        private static string PrintGreeting(string name, double salary)
+        {
+            return String.Format("Hello, {0}, your salary is {1}", name, Math.Ceiling(salary));
+        }
+    }
+}
