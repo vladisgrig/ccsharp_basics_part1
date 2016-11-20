22ba592aab9bb34a2ffde2fb8003dd96f1509045
vladisgrig <vladisgrig@yandex.ru>
2016-11-20 23:56:02 +0300

    String to number

---
+++ string_to_number/string_to_number/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_to_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string doubleNumber = "894376.243643";
+            double number = double.Parse(doubleNumber, CultureInfo.InvariantCulture);
+            Console.WriteLine(number + 1);
+        }
+    }
+}
