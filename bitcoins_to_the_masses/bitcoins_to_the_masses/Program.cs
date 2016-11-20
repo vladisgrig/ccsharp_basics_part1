1c82f3da9429730ac3d877b57eedb3002521f532
vladisgrig <vladisgrig@yandex.ru>
2016-11-20 23:43:43 +0300

    Cast in numbers

---
+++ bitcoins_to_the_masses/bitcoins_to_the_masses/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bitcoins_to_the_masses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вася регулярно получает за красивые глазки от кого-нибудь по amount биткоинов.
+            //Вася хочет знать, сколько биткоинов у него уже накопилось. А чтобы не мелочиться, хочет округлить сумму до ближайшего целого.
+            double amount = 1.11; //количество биткоинов от одного человека
+            int peopleCount = 60; // количество человек
+            int totalMoney = (int)Math.Round(amount * (double)peopleCount); // ← исправьте ошибку в этой строке
+            Console.WriteLine(totalMoney);
+        }
+    }
+}
