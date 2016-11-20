cade93a06dbe2b5b1ecd21d5c19e8f5609a5ac9a
vladisgrig <vladisgrig@yandex.ru>
2016-11-21 00:21:33 +0300

    Basics of methods

---
+++ main_question_in_universe/main_question_in_universe/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace main_question_in_universe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Print(GetSquare(42));
+        }
+
+        static int GetSquare(int x)
+        {
+            return x * x;
+        }
+
+        static void Print(int num)
+        {
+            Console.WriteLine(num.ToString());
+        }
+    }
+}
