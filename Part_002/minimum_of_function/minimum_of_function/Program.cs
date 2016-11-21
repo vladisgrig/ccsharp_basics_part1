5586aceccc620b7c97ce05b1e88d868c554d227c
vladisgrig <vladisgrig@yandex.ru>
2016-11-22 00:25:38 +0300

    Minimum of function

---
+++ Part_002/minimum_of_function/minimum_of_function/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minimum_of_function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteParabolaMinX(1, 2, 3);
+            WriteParabolaMinX(0, 3, 2);
+            WriteParabolaMinX(5, 2, 1);
+            WriteParabolaMinX(4, 3, 2);
+            WriteParabolaMinX(0, 4, 5);
+        }
+
+        private static void WriteParabolaMinX(int a, int b, int c)
+        {
+            if (a != 0)
+            {
+                double x = -b / (2.0 * a);
+                double y = a * x * x + b * x + c;
+                Console.WriteLine(x);
+            }
+            else
+            {
+                Console.WriteLine("Impossible");
+            }
+        }
+    }
+}
---
+++ Part_002/minimum_of_function/minimum_of_function/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("minimum_of_function")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("minimum_of_function")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("2f1025bd-8aec-496e-a70c-796fb6573d4f")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
