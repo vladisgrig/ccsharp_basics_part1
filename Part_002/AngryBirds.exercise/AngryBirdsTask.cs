59e810c96647989dc21ee11805606ba8503b9004
vladisgrig <vladisgrig@yandex.ru>
2016-11-22 23:01:32 +0300

    AngryBirds emulation

---
+++ Part_002/AngryBirds.exercise/AngryBirdsTask.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AngryBirds
+{
+	public static class AngryBirdsTask
+	{
+		//  Ниже — это XML документация, её использует ваша среда разработки, 
+		// чтобы показывать подсказки по использованию методов. 
+		// Но писать её естественно не обязательно.
+		/// <param name="v">Начальная скорость</param>
+		/// <param name="distance">Расстояние до цели</param>
+		/// <returns>Угол прицеливания в радианах от 0 до Pi/2</returns>
+		public static double FindSightAngle(double v, double distance)
+		{
+            double sin2alpha = distance * 9.8 / (v * v);
+            double angle = Math.Asin(sin2alpha) / 2;
+			return angle;
+		}
+	}
+}
