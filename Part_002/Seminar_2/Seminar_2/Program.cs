5e213eb2f00fa2d3fe6dcd1b46626d5b14a2ebd5
vladisgrig <vladisgrig@yandex.ru>
2016-11-23 23:56:31 +0300

    Seminar 2

---
+++ Part_002/Seminar_2/Seminar_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Expr10.Найти сумму всех положительных чисел меньше 1000 кратных 3 или 5.
+
+            //Expr11.Дано время в часах и минутах.Найти угол от часовой к минутной стрелке на обычных часах.
+
+            //Expr12. 1885.Комфорт пассажиров http://acm.timus.ru/problem.aspx?num=1885
+
+            //Expr13. 1084.Пусти козла в огород http://acm.timus.ru/problem.aspx?num=1084
+        }
+    }
+}
